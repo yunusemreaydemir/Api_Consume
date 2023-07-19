d84e4d85b56145c06c354df9a8209ae1c256003a
Yunus Emre Aydemir <yunusemre.aydemir@outlook.com>
2023-07-19 15:28:51 +0300

    model

--- Api_Consume/Controllers/CategoryController.cs
+++ Api_Consume/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Api_Consume.Dtos.CategoryDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace Api_Consume.Controllers
 {
@@ -22,6 +23,26 @@ namespace Api_Consume.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44325/api/Category", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
